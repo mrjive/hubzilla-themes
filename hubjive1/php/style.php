c7e9a5632d52864361ac1f9083cde40de730ae3f
mrjive <mrjive@mrjive.it>
2016-02-09 13:36:08 -0800

    ...

---
+++ hubjive1/php/style.php
@@ -0,0 +1,3 @@
+<?php
+require_once('view/theme/mrhub1/php/style.php');
+echo @file_get_contents('view/theme/mrhub1/css/style.css');
---
+++ hubjive1/php/theme.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *   * Name: mrhub1
+ *   * Description: A theme for Hubzilla. Extends the redbasic themes.
+ *   * Version: 0.1
+ *   * Author: mrjive
+ *   * Compat: Hubzilla [*]
+ *
+ */
+function mrhub1_init(&$a) {
+    $a->theme_info['extends'] = 'redbasic';
+}
